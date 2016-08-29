05eb2122ac95ec76f0e7f1a170c3fa15eeef0661
mhartington <mikehartington@gmail.com>
2016-08-29 17:59:49 -0400

    update

--- MyIonic2Project/typings/index.d.ts
+++ MyIonic2Project/typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="../plugins/cordova.plugin.zebra.tcp.printer/www/Zpl.d.ts" />
+/// <reference path="globals/cordova/index.d.ts" />
 /// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="modules/lodash/index.d.ts" />
-/// <reference path="../plugins/cordova.plugin.zebra.tcp.printer/www/Zpl.d.ts" />
